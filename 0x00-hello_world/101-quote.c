8d95bd8d957dd5528addef098b6d2ea97e88727b
buezman <chibuezenwajiobi@gmail.com>
2022-12-09 01:20:19 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,11 @@
 /**
  * main - program entry point
  * Return: returns 1 (success)
- *
- *
  */
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
-	write(1, str, 59);
+	write(1,str,59);
 	return (1);
 }
