931c241688022feb33117432f8d863aa1aae89c9
buezman <chibuezenwajiobi@gmail.com>
2022-12-08 18:52:18 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
-int main(void) {
+/**
+ * main -program Entry point
+ * puts function prints argument as output
+ * program returns 0 - success
+ *
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
