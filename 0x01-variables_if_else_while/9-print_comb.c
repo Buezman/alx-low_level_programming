6abb058ea03af6a3d46a9093863f3de26ecc5179
buezman <chibuezenwajiobi@gmail.com>
2022-12-09 08:53:12 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all comination of single digit numbers
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
