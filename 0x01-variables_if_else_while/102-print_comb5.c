7883f120817d511c943f865505d55d9416019e2c
buezman <chibuezenwajiobi@gmail.com>
2022-12-10 00:13:27 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits combo
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	int i , j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			putchar(i + '0');
+			putchar(i + '0');
+			putchar(' ');
+			putchar(j + '0');
+			putchar(j + 1 + '0');	
+		}
+	}
+	putchar('\n');
+	return (0);
+}
