dd527c9a98b39ed4e526069cadd9ee5ff844d55d
buezman <chibuezenwajiobi@gmail.com>
2022-12-13 20:29:25 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	int num[] = "72, 11, 108, 98, 101, 114, 116, 111, 110";
-	int i, count, l;
+	int num[] = {72, 11, 108, 98, 101, 114, 116, 111, 110};
+	int i, l;
 
 	l = sizeof(num) / sizeof(int);
 	for (i = 0; i < l; i++)
