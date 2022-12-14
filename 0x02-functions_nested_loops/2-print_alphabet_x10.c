dad633b8ac34b93ff9f53ffc03ca35dd556ec9ea
buezman <chibuezenwajiobi@gmail.com>
2022-12-13 21:01:55 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		print_alphabet(void);
+		for (j = 97; j < 123; j++)
+		{
+			_putchar(j);
+		}
 	}
 	_putchar('\n');
 }
