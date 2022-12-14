f985dbd1e1fc7849259ce17dcebf36c381b0905c
buezman <chibuezenwajiobi@gmail.com>
2022-12-13 20:59:10 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints lowercase alphabets 10 times
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet(void);
+	}
+	_putchar('\n');
+}
