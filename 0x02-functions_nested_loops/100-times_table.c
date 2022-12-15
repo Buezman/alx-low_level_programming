7efe2ded001210025d012fcd467610a0bd05ac8b
buezman <chibuezenwajiobi@gmail.com>
2022-12-15 09:43:48 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,6 @@
  * @n: positive integer
  *
  * Return: 0 (success)
- *
  */
 void print_times_table(int n)
 {
@@ -19,13 +18,9 @@ void print_times_table(int n)
 			{
 				x = i * j;
 				if (j > 0 && x < 100)
-				{
 					_putchar(' ');
-				}
 				if (j > 0 && x < 10)
-				{
 					_putchar(' ');
-				}
 				if (x > 99)
 				{
 					_putchar((x / 100) + '0');
@@ -38,9 +33,7 @@ void print_times_table(int n)
 					_putchar((x % 10) + '0');
 				}
 				else
-				{
 					_putchar(x + '0');
-				}
 				if (j < n)
 				{
 					_putchar(',');
