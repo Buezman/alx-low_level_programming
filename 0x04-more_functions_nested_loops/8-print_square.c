b9b131c3b16f726ebfc4db00eee09dab7504961e
buezman <chibuezenwajiobi@gmail.com>
2022-12-15 18:49:25 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,23 +2,23 @@
 
 /**
  * print_square - prints a square of side n using #
- * @n: intger
+ * @size: intger
  * Return: 0 (success)
  */
 
-void print_square(int n)
+void print_square(int size)
 {
 	int i, j;
 	
-	if (n > 0)
+	if (size > 0)
 	{
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < size; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
-			if (i < (n - 1))
+			if (i < (size - 1))
 			{
 				_putchar('\n');
 			}
