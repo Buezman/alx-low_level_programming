2f906c4ac307977e4d84932bc3d0824c4afd3c60
buezman <chibuezenwajiobi@gmail.com>
2023-01-05 08:56:19 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,17 @@
  */
 void print_rev(char *str)
 {
-	int l = _strlen(str);
+	int l = 0;
+
+	while (*(str + l) != '\0')
+	{
+		l++;
+	}
 
 	while (l >= 0)
 	{
 		_putchar(str[l]);
+		l--;
 	}
 	_putchar('\n');
 }
