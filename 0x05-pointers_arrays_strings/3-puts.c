176d436c8d215ad2f7a48ecd8c95e4be411eb012
buezman <chibuezenwajiobi@gmail.com>
2023-01-05 08:41:18 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a given string to stdout
+ * @str: given string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int n = 0;
+
+	while(str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
