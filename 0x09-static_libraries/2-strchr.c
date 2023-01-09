1eeede0d12731bb429376c3bd790accf0e465e19
buezman <chibuezenwajiobi@gmail.com>
2023-01-09 09:56:38 -0800

    add strchr function

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strchr - finds a character in a string
+ * @s: input string
+ * @c: character to be located
+ * Return: returns the pointer to first occurence of c
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		else
+			i++;
+	}
+	return ('\0');
+}
