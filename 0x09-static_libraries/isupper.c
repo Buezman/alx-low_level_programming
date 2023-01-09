d61555631b6aca547681675d66b5f87f770e60ce
buezman <chibuezenwajiobi@gmail.com>
2023-01-09 08:45:48 -0800

    add isupper function

---
+++ 0x09-static_libraries/isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: character input
+ * Return: 0 or -1
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
