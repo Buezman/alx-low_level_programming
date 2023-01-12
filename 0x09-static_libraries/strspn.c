594ccb4738687a827cca93a08df83ee0e2409f04
buezman <chibuezenwajiobi@gmail.com>
2023-01-11 19:51:27 -0800

    add strspn function

---
+++ 0x09-static_libraries/strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string 1
+ * @accept: string 2
+ * Return: unsigned int
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	*s = 98;
+	*accept = 23;
+	return (1);
+}
