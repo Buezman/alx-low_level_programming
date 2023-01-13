04d96fd24d9dc364f79487918f63529bdc1d30eb
buezman <chibuezenwajiobi@gmail.com>
2023-01-13 11:43:46 -0800

    add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that prints it's method name
+ * @argc: number of command line arguments
+ * @argv: array of commandline arguments
+ * Return: 0
+ *
+ */
+int main(int argc void, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
