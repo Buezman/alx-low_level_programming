bb152a7bba03f52e9c8d979698ff7f37b7555980
buezman <chibuezenwajiobi@gmail.com>
2023-01-19 08:54:44 -0800

    add 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - prints parameters seperated by a delimeter
+ * @*seperator: delimeter between parameters
+ * @n: number of parameters
+ * @...: parameters
+ * Return: String of parameters seperated by delimeter
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list chars;
+	unsigned int i;
+
+	va_start(chars, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(chars, int));
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	
+	va_end(chars);
+}
