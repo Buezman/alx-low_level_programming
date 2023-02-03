18abcf3eb99ab590f4975e126dc6796008770664
buezman <chibuezenwajiobi@gmail.com>
2023-02-02 22:54:24 -0800

    add 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercase letters in a string to uppercase
+ * @str: given str
+ * Return: manipulated string
+ *
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+
+	return (str);
+}
