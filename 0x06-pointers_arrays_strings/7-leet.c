16b95cef1636f25018fe363d6b093de3131cd8a2
buezman <chibuezenwajiobi@gmail.com>
2023-02-03 00:31:45 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,16 +9,16 @@ char *leet(char *str)
 {
 	int i = 0, j;
 
-	char strArr[5] = {"a", "e", "o", "t", "l};
+	char strArr[5] = {'a', 'e', 'o', 't', 'l'};
 
-	int intArr[5] = {4, 3, 0, 7, 1};
+	char intArr[5] = {'4', '3', '0', '7', '1'};
 
 	while (str[i])
 	{
 		for (j = 0; j < 5; j++)
 		{
 			if (str[i] == strArr[j] || str[i] + 32 == strArr[j])
-				str[i] = intArr[j] + '0';
+				str[i] = intArr[j];
 		}
 		i++;
 	}
