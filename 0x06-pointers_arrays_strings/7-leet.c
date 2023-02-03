c75e5b0c02ccb39e7cf156c3f3f44d622b4dbd70
buezman <chibuezenwajiobi@gmail.com>
2023-02-03 00:24:32 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - encodes a string to 1337
+ * @str: given string
+ * Return: encoded string
+ *
+ */
+char *leet(char *str)
+{
+	int i = 0, j;
+
+	char strArr[5] = {"a", "e", "o", "t", "l};
+
+	char intArr[5] = {4, 3, 0, 7, 1};
+
+	while (str[i])
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == strArr[j] || str[i] + 32 == strArr[j])
+				str[i] = intArr[j];
+		}
+		i++;
+	}
+	return (str);
+}
