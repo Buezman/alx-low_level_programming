33609a969a7d73d29701f018caf9725827855539
buezman <chibuezenwajiobi@gmail.com>
2023-02-03 08:30:57 -0800

    add 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - prints a number
+ * @n: given number
+ * Return: void
+ *
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if ((i / 10) > 0)
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
+}
