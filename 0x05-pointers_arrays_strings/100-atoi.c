5e41b97a90748903c607959d5cdf8055cb5ee3eb
buezman <chibuezenwajiobi@gmail.com>
2023-02-10 09:23:07 -0800

    add 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,14 +10,12 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int n = 0;
 
-	while (*(s++))
+	while (*(s++) && n <= 0)
 	{
 		if (*s == '-')
 			sign *= -1;
 		else if (*s >= 0 && *s <= 9)
 			n = (n * 10) + (*s - '0');
-		else
-			continue;
 	}
 
 	return (n * sign);
