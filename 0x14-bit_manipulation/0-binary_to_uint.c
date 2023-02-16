e923f6fe62f49382fc92f6db31aa7f64d17b3f6a
buezman <chibuezenwajiobi@gmail.com>
2023-02-16 11:07:26 -0800

    add 0-binary_to_unint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,8 +8,8 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i, n = 0, x = 1;
-	unsigned int num = 0;
+	int i, n = 0;
+	unsigned int x = 1, num = 0;
 
 	if (b == NULL)
 		return (0);
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = n - 1; i >= 0; i--)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
 		num += (b[i] - '0') * x;
