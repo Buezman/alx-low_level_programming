d5d1c50fa22c5301d9f13bf4093b2c7432b23fcc
buezman <chibuezenwajiobi@gmail.com>
2023-02-16 11:58:51 -0800

    add 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary form of a number
+ * @n: number to print
+ * Return: void
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	while (n > 1)
+	{
+		_putchar((n & 1) + '0');
+		n >> 1;
+	}
+}
