d43240f4dfe04985695221b11e7fcdb577d5fdc0
buezman <chibuezenwajiobi@gmail.com>
2023-02-23 00:33:10 -0800

    add get_endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - determines endianness
+ * Return: if big-endian 1, otherwise 0
+ *
+ */
+int grt_endianness(void)
+{
+	int num = 1;
+	char *e = (char *)&num;
+
+	if (*e == 1)
+		return (1);
+
+	return (0);
+}
