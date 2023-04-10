d1ec5d278aa4b76cccbc7d73df6b511d4d918b02
buezman <chibuezenwajiobi@gmail.com>
2023-04-10 14:42:03 -0700

    add 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all elements in alinked list
+ * @h: linked list
+ * Return: number of elements in the linked list
+ *
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n)
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
